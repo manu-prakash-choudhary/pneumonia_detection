dadc47c724866c9237bf8fd857708be44cbb3968
manuprakash <manu.prakash2020@vitbhopal.ac.in>
2023-03-22 08:40:50 +0530

    changes

--- pneumonia_detection/urls.py
+++ pneumonia_detection/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pneumonia_home.urls')),
+    path('', include(('pneumonia_home.urls', 'pneumonia_home'), namespace='pneumonia_home')),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pneumonia_home/urls.py
+++ pneumonia_home/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/<str:home>', views.index, name='index'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
